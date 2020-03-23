d65b9caf0d09c3f5aebed51e95107d39b7720ece
Tobias Thaden <hello@tobiasthaden.com>
2020-03-23 21:40:38 +0100

    Install dotfiles

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Removes .zshrc from $HOME and symlinks the .zshrc file from the .dotfiles
+rm -rf $HOME/.zshrc
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+
+# Symlink the Mackup config file to the home directory
+ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
+
+# Set macOS preferences
+source .macos
