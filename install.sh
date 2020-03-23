9339cfe5781c40d37935be2d93161039d7ce9da6
Tobias Thaden <hello@tobiasthaden.com>
2020-03-23 21:58:46 +0100

    Add homebrew

--- install.sh
+++ install.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
+# Check for Homebrew and install if we don't have it
+if test ! $(which brew); then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update Homebrew recipes
+brew update
+
+# Install all our dependencies with bundle (See Brewfile)
+brew tap homebrew/bundle
+brew bundle
+
+# Set default MySQL root password and auth type.
+mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
+
+# Create a Sites directory
+mkdir $HOME/Sites
+
 # Removes .zshrc from $HOME and symlinks the .zshrc file from the .dotfiles
 rm -rf $HOME/.zshrc
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
