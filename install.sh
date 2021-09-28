28c10a1e84411eb8d7e55edd307a8b1d42caae89
Tobias Thaden <hello@tobiasthaden.com>
2021-09-28 21:42:49 +0200

    Remove 'mysql' in favor of 'docker'

--- install.sh
+++ install.sh
@@ -12,9 +12,6 @@ brew update
 brew tap homebrew/bundle
 brew bundle
 
-# Set default MySQL root password and auth type.
-mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
-
 # Create a Sites directory
 mkdir $HOME/Sites
 
