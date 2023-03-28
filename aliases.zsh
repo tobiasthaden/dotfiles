937a38b36aa83c3032e4d188edbb373d7bf36ba2
Tobias Thaden <hello@tobiasthaden.com>
2023-03-28 14:24:57 +0200

    Add sail:init alias

--- aliases.zsh
+++ aliases.zsh
@@ -1,3 +1,5 @@
+DOCKER_PHP_IMAGE=laravelsail/php81-composer:latest
+
 alias copy:ssh="pbcopy < $HOME/.ssh/id_rsa.pub"
 alias reload:shell="source $HOME/.zshrc"
 
@@ -11,6 +13,13 @@ alias composer:fresh="rm -rf vendor/ composer.lock && composer install"
 
 # Laravel Sail
 alias sail='[ -f sail ] && bash sail || bash vendor/bin/sail'
+
+alias sail:init='docker run --rm \
+    -u "$(id -u):$(id -g)" \
+    -v $(pwd):/var/www/html \
+    -w /var/www/html \
+    $DOCKER_PHP_IMAGE \
+    composer install --ignore-platform-reqs'
 
 # JS
 alias node:fresh="rm -rf node_modules/ package-lock.json && npm install"
