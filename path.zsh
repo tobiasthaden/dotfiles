eedeec742587f6ba46e234e122afc55049f5a518
Tobias Thaden <hello@tobiasthaden.com>
2023-08-09 18:30:54 +0200

    Load nvm with bash completion

--- path.zsh
+++ path.zsh
@@ -7,3 +7,8 @@ export PATH="$HOME/.node/bin:$PATH"
 # Use project specific binaries before global ones
 export PATH="node_modules/.bin:vendor/bin:$PATH"
 
+# Load NVM with bash completion
+export NVM_DIR="$HOME/.nvm"
+  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"
+  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
+
